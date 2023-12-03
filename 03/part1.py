7576ea496ced80166d50b88227d2119ad951a89f
simon brahan <tntpiranha@gmail.com>
2023-12-03 17:33:26 +0000

    Day 3 part 1 works on sample input, wrong on actual input

---
+++ 03/part1.py
@@ -0,0 +1,65 @@
+def parseSchematic(fileName):
+    numbers = []
+    symbols = set()
+    with open(fileName) as file:
+        for y, line in enumerate(file):
+            lineNumbers, lineSymbols = parseLine(line.strip(), y)
+            numbers += lineNumbers
+            symbols.update(lineSymbols)
+
+    return numbers, symbols
+
+
+def parseLine(line, y):
+    numbers = []
+    symbols = set()
+
+    currentDigits = []
+    for idx, char in enumerate(line):
+        if char.isdigit():
+            currentDigits.append(char)
+            continue
+
+        if len(currentDigits) > 0:
+            numbers.append(
+                {
+                    'number': int(''.join(currentDigits)),
+                    'start': (idx-len(currentDigits), y),
+                }
+            )
+
+            currentDigits = []
+
+        if char != '.':
+            symbols.add((idx, y))
+
+    return numbers, symbols
+
+
+def isPartNumber(number, symbols):
+    for space in adjacentSpaces(number):
+        if space in symbols:
+            return True
+
+    return False
+
+
+def adjacentSpaces(number):
+    length = len(str(number['number']))
+    startX, startY = number['start']
+    above = [(x, startY-1) for x in range(startX-1, startX+length+1)]
+    left = [(startX-1, startY)]
+    right = [(startX+length, startY)]
+    below = [(x, startY+1) for x in range(startX-1, startX+length+1)]
+
+    return above + below + left + right
+
+
+numbers, symbols = parseSchematic('input.txt')
+
+partNumbers = []
+for number in numbers:
+    if isPartNumber(number, symbols):
+        partNumbers.append(number['number'])
+
+print(sum(partNumbers))
