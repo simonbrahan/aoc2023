96869c12fd671ed20402b12e4dd39a46b59f9be6
simon brahan <tntpiranha@gmail.com>
2023-12-07 09:50:23 +0000

    Day 6 part 2 completed in python

---
+++ 06/part2.py
@@ -0,0 +1,29 @@
+import math
+
+def parseRaces(fileName):
+    with open(fileName) as file:
+        times, recordDistances = list(map(numsFromLine, file.readlines()))
+
+    return list(zip(times, recordDistances))
+
+
+def numsFromLine(line):
+    return [int(''.join(filter(lambda char: char.isdigit(), line)))]
+
+
+def countWaysToWin(time, recordDistance):
+    out = 0
+    for i in range(time):
+        if i * (time - i) > recordDistance:
+            out += 1
+
+    return out
+
+
+races = parseRaces('input.txt')
+
+numWaysToWin = []
+for time, recordDistance in races:
+    numWaysToWin.append(countWaysToWin(time, recordDistance))
+
+print(math.prod(numWaysToWin))
